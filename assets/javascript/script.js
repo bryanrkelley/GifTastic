82351054f0e1be3fd630aca868ae949da511acd9
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-01-26 15:21:30 -0800

    pause/play functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,14 @@
 $(document).ready(() => {
 
-    //var $animalButtons = $('#animalButtons');
+    var $animalButtons = $('#animalButtons');
 
 
     $('#userInput').on('click', function () {
         $('#userInput').val('');
     });
 
-    $('#userButton').on('click', function (event) {
+
+    var userButton = function (event) {
         event.preventDefault();
 
         var newButton = $('#userInput').val().trim();
@@ -15,24 +16,26 @@ $(document).ready(() => {
 
         $('#userInput').val('');
 
-        $('#animalButtons').append('<button class="buttons" data-animal="' + newButton + '">' + newButton + '</button>')
+        $animalButtons.append('<button class="buttons" data-animal="' + newButton + '">' + newButton + '</button>');
+
+        $('.input-field').val("")
+    };
+
+    //Hitting the button will submit
+    $('#userButton').on('click', function (event) {
+        userButton(event);
     });
 
     //Want to make it so hitting enter will cause submission too
 
-    // $('#userButton').keyup(function (event) {
-    //     if (event.keyCode === 13) {
+    $('#userInput').keyup(function (event) {
+        console.log(event)
+        if (event.keyCode === 13) {
+            userButton(event);
+        }
+    });
 
-    //         var newButton = $('#userInput').val().trim();
-    //         console.log("new button: " + newButton);
-
-    //         $('#userInput').val('');
-
-    //         $('#animalButtons').append('<button class="buttons" data-animal="' + newButton + '">' + newButton + '</button>')
-    //     }
-    // });
-
-    $('#animalButtons').on('click', 'button', function () {
+    $animalButtons.on('click', 'button', function () {
         console.log('buttons');
         var animal = $(this).attr("data-animal");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -54,8 +57,8 @@ $(document).ready(() => {
                 var p = $("<p>").text("Rating: " + rating);
                 var animalImage = $("<img>");
                 console.log(results[i]);
-                animalImage.attr("src", results[i].images.fixed_height.url);
-                animalImage.attr("data-still", results[i].images.fixed_height.url);
+                animalImage.attr("src", results[i].images.fixed_height_still.url);
+                animalImage.attr("data-still", results[i].images.fixed_height_still.url);
                 animalImage.attr("data-animate", results[i].images.fixed_height.url);
 
                 animalImage.addClass("gif");
@@ -71,19 +74,24 @@ $(document).ready(() => {
     });
 
 
-    //Pause functionality.  Not working
-    $(document).on("click", ".gif", function () {
+    //Pause functionality via mouse over events
+    $(document).on("mouseenter", ".gif", function () {
         var state = $(this).attr('data-state');
+        // console.log(state)
 
-        if (state === "still") {
-            $(this).attr('src', $(this).attr('data-animate'));
-            $(this).attr('data-state', 'animate');
-        } else {
-            $(this).attr('src', $(this).attr('data-still'));
-            $(this).attr('data-state', 'still');
-
-        }
-
+        // if (state === "still") {
+        $(this).attr('src', $(this).attr('data-animate'));
+        $(this).attr('data-state', 'animate');
+        // } else {
+        //     $(this).attr('src', $(this).attr('data-still'));
+        //     $(this).attr('data-state', 'still');
+        // }
     });
 
+    $(document).on("mouseleave", ".gif", function () {
+        var state = $(this).attr('data-state');
+        // console.log(state)
+        $(this).attr('src', $(this).attr('data-still'));
+        $(this).attr('data-state', 'still');
+    });
 });
